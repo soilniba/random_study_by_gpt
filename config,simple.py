b87400ff6b5ff8cdb9c6fbde512b3e0ccdf75f86
王畅 <wangchang@wangyuan.com>
2023-03-27 16:29:36 +0800

    配置文件模板

---
+++ config,simple.py
@@ -0,0 +1,11 @@
+wx_robot_error = ''
+wx_robot_study = ''
+
+feishu_robot_error = ''
+feishu_robot_study = ''
+
+feishu_app_id = ''
+feishu_app_secret = ''
+
+openai_api_key = ''
+azure_api_key = ''
